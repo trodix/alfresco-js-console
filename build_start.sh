15febd66a2882ceb2dc760c16766b5a8a1f0e310
Sébastien Vallet <sebastien.vallet89@gmail.com>
2022-05-01 00:46:46 +0200

    migration from alfreco-js-api to @alfresco/js-api

--- build_start.sh
+++ build_start.sh
@@ -2,5 +2,6 @@
 
 npm run build
 
+docker-compose build --no-cache
 docker-compose up --force-recreate
 docker-compose logs -f
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { MonacoEditorLoaderService, MonacoStandaloneCodeEditor } from '@materia-ui/ngx-monaco-editor';
-import * as AlfrescoApi from 'alfresco-js-api';
+import { AlfrescoApi, AlfrescoApiConfig, NodesApi } from '@alfresco/js-api';
 import { filter, take } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
@@ -18,7 +18,7 @@ export interface EditorContext {
     warn(msg: string | object): void,
     error(msg: string | object): void
   },
-  alfrescoJsApi: AlfrescoApi
+  nodesApi: NodesApi
 }
 
 @Component({
@@ -36,7 +36,7 @@ export class AppComponent {
   @ViewChild("logContainer")
   public logContainer!: ElementRef;
 
-  public alfrescoJsApi = new AlfrescoApi({ provider:'ECM', hostEcm: 'http://localhost:8090' });
+  public alfrescoJsApi = new AlfrescoApi(new AlfrescoApiConfig({ provider: 'ECM', hostEcm: "http://localhost:8090" }));
 
   constructor(private monacoLoaderService: MonacoEditorLoaderService, private http: HttpClient) {}
 
@@ -104,33 +104,34 @@ export class AppComponent {
 
   getCode() {
     return (
-      `
-      const fileOrFolderId = '80a94ac8-3ece-47ad-864e-5d939424c47c';
+`
 
-      this.alfrescoJsApi.nodes.getNodeInfo(fileOrFolderId).then(data => {
-          this.logger.info('This is the name: ' + data.name );
-      }, error => {
-          this.logger.error('This node does not exist');
-      });
+const fileOrFolderId = '80a94ac8-3ece-47ad-864e-5d939424c47c';
 
-      var folderNodeId = '80a94ac8-3ece-47ad-864e-5d939424c47c';
-      
-      this.alfrescoJsApi.nodes.getNodeChildren(folderNodeId).then(data => {
-          this.logger.info('The number of children in this folder are ' + data.list.pagination.count);
-          data.list.entries.forEach(node => {
-              this.logger.info(node.entry.id + " - " + node.entry.name);
-          });
-      }, error => {
-          this.logger.error('This node does not exist');
-      });
+this.nodesApi.getNode(fileOrFolderId).then(node => {
+    this.logger.info('This is the name: ' + node.entry.name );
+}, error => {
+    this.logger.error('This node does not exist');
+});
 
-      `
+const folderNodeId = '80a94ac8-3ece-47ad-864e-5d939424c47c';
+
+this.nodesApi.listNodeChildren(folderNodeId).then(data => {
+    this.logger.info('The number of children in this folder are ' + data.list.pagination.count);
+    data.list.entries.forEach(node => {
+        this.logger.info(node.entry.id + " - " + node.entry.name);
+    });
+}, error => {
+    this.logger.error('This node does not exist');
+});
+
+`
     );
   }
 
   run() {
     const contextEditor: EditorContext = {
-      alfrescoJsApi: this.alfrescoJsApi,
+      nodesApi: new NodesApi(this.alfrescoJsApi),
       logger: {
         log: (msg: string | object) => {
           this.logs.push({ type: 'log', timestamp: new Date(), msg });
